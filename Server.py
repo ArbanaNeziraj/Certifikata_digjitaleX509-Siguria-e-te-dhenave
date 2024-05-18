9af1531d8a6a0819c9c5329621b5f056cfe57add
ArbanaNeziraj <116509042+ArbanaNeziraj@users.noreply.github.com>
2024-05-18 23:20:02 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,9 @@
 import OpenSSL
 import socket as soct
 import ssl
+import hashlib
+import base64
+from cryptography.fernet import Fernet
 
 # Ngarkoni certifikatën dhe çelësin SSL
 openssl = OpenSSL.crypto
@@ -31,5 +34,33 @@ while True:
     clientSocket, addr = serverSocket.accept()
     print('Received connection from:', addr)
     sslClientSocket = context.wrap_socket(clientSocket, server_side=True)
+
+# Gjenero celesin e Enkriptimit
+    key1= hashlib.sha256()
+    key1.update(soct.gethostname().encode('utf-8'))
+    key2 = key1.hexdigest()[:32]
+    key3 = base64.b64encode(key2.encode('utf-8'))
+    cipher_suite = Fernet(key3)
+
+    while True:
+        # Receive and decrypt message from client
+        received = sslClientSocket.recv(1024)
+        if not received:
+            break
+        decrypted = cipher_suite.decrypt(received)
+        print("\n>> Message sent from client (encrypted):", received)
+        print(">> Decrypted message sent from client:", decrypted.decode('utf-8'))
+
+        if decrypted.decode('utf-8').lower() == "bye":
+            break
+
+# Merrni përgjigje nga përdoruesi i serverit, kriptoni dhe dërgoni përsëri
+        message = input('Enter message for client: ')
+        encoded_text = cipher_suite.encrypt(message.encode('utf-8'))
+        sslClientSocket.send(encoded_text)
+        
+    sslClientSocket.close()
+    if decrypted.decode('utf-8').lower() == "bye":
+        break
 
     serverSocket.close()
