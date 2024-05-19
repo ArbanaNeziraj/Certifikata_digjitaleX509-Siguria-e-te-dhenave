a731cf52b5ea6f2546f000d19ae3beb26abbae98
Alisa-Shala <alisa.shala@student.uni-pr.edu>
2024-05-19 15:18:52 +0200

    update client

--- Client.py
+++ Client.py
@@ -27,14 +27,20 @@ key2 = key1.hexdigest()[:32]
 key3 = base64.b64encode(key2.encode('utf-8'))
 cipher_suite = Fernet(key3)
 try:
-  while true:
-    message = input('Enter message foer server:')
-    encrypted_message = cipher_suite.encrypt(message.encode('utf-8))
+  while True:
+    message = input('Enter message for server: ')
+    encrypted_message = cipher_suite.encrypt(message.encode('utf-8'))
     ssl_sock.sendall(encrypted_message)
 
-    if message.lower() == "bye";
-    break
-    response =ssl_sock.recv(1024)
+    if message.lower() == "bye":
+      break
+
+    # Receive and decrypt server response
+    response = ssl_sock.recv(1024)
     decrypted_response = cipher_suite.decrypt(response)
-    print("\n>>Message sent from server(encrypted);",response)
-    print(">> Decrypted message sent from server:",decrypted_response.decode(utf-8))
+    print("\n>> Message sent from server (encrypted):", response)
+    print(">> Decrypted message sent from server:", decrypted_response.decode('utf-8'))
+
+finally:
+  print("Closing connection")
+  ssl_sock.close()
